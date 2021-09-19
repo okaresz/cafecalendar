cbf4f58d2677c6d6f9e0f3b5f4c476a5f77842ed
MartinJM <martinjefmeyers@gmail.com>
2021-09-19 11:23:11 +0200

    Switched to newer version of fullcalendar to fix recurring events.

--- htdocs/script.js
+++ htdocs/script.js
@@ -1,64 +1,50 @@
-const mapping = {
-  dtstart: "start",
-  dtend: "end",
-  summary: "title",
-};
-
-const value_type_mapping = {
-  "date-time": (input) => {
-    if (input.substr(-3) === "T::") {
-      return input.substr(0, input.length - 3);
-    }
-    return input;
-  },
-};
-
-function load_ics(ics_data) {
-  const parsed = ICAL.parse(ics_data);
-  const events = parsed[2].map(([type, event_fields]) => {
-    if (type !== "vevent") return;
-    return event_fields.reduce((event, field) => {
-      const [original_key, _, type, original_value] = field;
-      const key =
-        original_key in mapping ? mapping[original_key] : original_key;
-      const value =
-        type in value_type_mapping
-          ? value_type_mapping[type](original_value)
-          : original_value;
-      event[key] = value;
-      return event;
-    }, {});
-  });
-  $("#calendar").fullCalendar("removeEventSources");
-  $("#calendar").fullCalendar("addEventSource", events);
-}
+var calendar;
 
 function fetch_ics_feed(url) {
-  $.get(url, (res) => load_ics(res));
-  if ($("#share input")[0].checked) {
-    URIHash.set('feed', url);
-  } else {
-    URIHash.set('feed', '');
-    window.location.hash = "";
-  }
+  calendar.removeAllEventSources()
+  calendar.addEventSource(
+    {
+      url: url,
+      format: 'ics'
+    }
+  );
 }
 
 $(document).ready(function () {
-  $("#calendar").fullCalendar({
-    header: {
-      left: "prev,next today",
-      center: "title",
-      right: "month,agendaWeek,agendaDay,listMonth",
-    },
-    navLinks: true,
-    editable: false,
-    minTime: "0:00:00",
-    maxTime: "24:00:00",
-    firstDay: 1,
-    nowIndicator: true,
-    slotLabelFormat: "HH:mm",
-    eventTimeFormat: "HH:mm",
-  });
+  calendar = new FullCalendar.Calendar(
+    document.getElementById('calendar'),
+    {
+      initialView: 'dayGridMonth',
+      headerToolbar: {
+        start: "prev,next today",
+        center: "title",
+        end: "dayGridMonth,timeGridWeek,timeGridDay,listMonth"
+      },
+      navLinks: true,
+      editable: false,
+      slotMinTime: "0:00:00",
+      slotMaxTime: "24:00:00",
+      firstDay: 1,
+      nowIndicator: true,
+      slotLabelFormat: {
+        hour: "numeric",
+        minute: "2-digit",
+        omitZeroMinute: false,
+        meridiem: false,
+        hour12: false
+      },
+      eventTimeFormat: {
+        hour: "numeric",
+        minute: "2-digit",
+        omitZeroMinute: false,
+        meridiem: false,
+        hour12: false
+      }
+    }
+  );
+
+  calendar.render();
+
   const url_feed = URIHash.get("feed");
   const url_title = URIHash.get("title");
   console.log({
